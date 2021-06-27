3b1b3c3216817c300d805cf739a7b8f1340a4b93
Patrick <pwfenger@gmail.com>
2021-06-28 00:53:41 +0200

    Challenge2

---
+++ Chapters/Chapter 1/second_program.py
@@ -0,0 +1,6 @@
+
+season = input('Which season is your favorite time of year?')
+number = input('What is your lucky number?')
+adjective = input('Choose one of the following adjectives: Warm, Cold, Hot, Chilly, Dark')
+
+print(f'On a {adjective}{season} day, I drink at least {number} cups of juice!')
